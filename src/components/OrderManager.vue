144a2a25a8976be3117804d4a3a2c371b68fc653
begimov <begimov@gmail.com>
2018-10-25 16:15:35 +0300

    basic orders listing implemented

--- src/components/OrderManager.vue
+++ src/components/OrderManager.vue
@@ -5,7 +5,32 @@
         <div class="panel-heading">
           <h3 class="panel-title">Менеджер заказов</h3>
         </div>
-        <div class="panel-body">//</div>
+        <div class="panel-body">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>номер заказа</th>
+                <th>имя</th>
+                <th>товары</th>
+                <th>сумма</th>
+                <th>статус</th>
+                <th>адрес</th>
+                <th>комментарий</th>
+              </tr>
+            </thead>
+            <tbody>
+                <tr v-for="order in orders" :key="order.order_id">
+                  <th scope="row">{{order.order_id}}</th>
+                  <th>{{order.customer}}</th>
+                  <td>{{order.product}}}</td>
+                  <td>{{order.total}}</td>
+                  <td>{{order.order_status}}</td>
+                  <td>{{ order.shipping_city + ' ' + order.shipping_address_1 }}</td>
+                  <td>{{order.comment}}</td>
+                </tr>
+              </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
@@ -36,13 +61,14 @@ export default {
           limit
         }
       }).then(res => {
-        this.orders = res
+        this.orders = res.data
+        console.log(this.orders)
       })
     }
   },
   mounted() {
     this.getOrders(1, 10)
-    this.getOrders(2, 10)
   }
 }
 </script>
+
