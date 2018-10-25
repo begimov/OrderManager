3aff911e013f48207a5223e10886a974b547c97c
begimov <begimov@gmail.com>
2018-10-25 16:37:34 +0300

    orders list moved to separate component

---
+++ src/components/OrderList.vue
@@ -0,0 +1,41 @@
+<template>
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>номер заказа</th>
+        <th>имя</th>
+        <th>товары</th>
+        <th>сумма</th>
+        <th>статус</th>
+        <th>адрес</th>
+        <th>комментарий</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr v-for="order in orders" :key="order.order_id">
+          <th scope="row">{{order.order_id}}</th>
+          <th>{{order.customer}}</th>
+          <td>{{order.product}}}</td>
+          <td>{{order.total}}</td>
+          <td>{{order.order_status}}</td>
+          <td>{{ order.shipping_city + ' ' + order.shipping_address_1 }}</td>
+          <td>{{order.comment}}</td>
+        </tr>
+      </tbody>
+  </table>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'OrderList',
+  props: {
+    orders: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
+
--- src/components/OrderManager.vue
+++ src/components/OrderManager.vue
@@ -6,30 +6,7 @@
           <h3 class="panel-title">Менеджер заказов</h3>
         </div>
         <div class="panel-body">
-          <table class="table table-bordered">
-            <thead>
-              <tr>
-                <th>номер заказа</th>
-                <th>имя</th>
-                <th>товары</th>
-                <th>сумма</th>
-                <th>статус</th>
-                <th>адрес</th>
-                <th>комментарий</th>
-              </tr>
-            </thead>
-            <tbody>
-                <tr v-for="order in orders" :key="order.order_id">
-                  <th scope="row">{{order.order_id}}</th>
-                  <th>{{order.customer}}</th>
-                  <td>{{order.product}}}</td>
-                  <td>{{order.total}}</td>
-                  <td>{{order.order_status}}</td>
-                  <td>{{ order.shipping_city + ' ' + order.shipping_address_1 }}</td>
-                  <td>{{order.comment}}</td>
-                </tr>
-              </tbody>
-          </table>
+          <OrderList :orders="orders"/>
         </div>
       </div>
     </div>
@@ -38,9 +15,13 @@
 
 <script>
 import axios from 'axios'
+import OrderList from './OrderList.vue'
 
 export default {
   name: 'OrderManager',
+  components: {
+    OrderList
+  },
   props: {
     token: {
       required: true,
