6c5dbc1e6c6f4bdfcd98bdbb83984b1603f40aa8
begimov <begimov@gmail.com>
2018-10-25 13:31:13 +0300

    basic prototyping done

--- src/components/OrderManager.vue
+++ src/components/OrderManager.vue
@@ -1,31 +1,48 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="panel-title">Менеджер заказов</h3>
-          </div>
-          <div class="panel-body"><a :href="url">ACCESS TOKEN</a></div>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Менеджер заказов</h3>
         </div>
+        <div class="panel-body">//</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'OrderManager',
   props: {
     token: {
       required: true,
-      type:String
+      type: String
     }
   },
-  computed: {
-    url() {
-      return `http://starbucksshopru.test/admin/index.php?route=common/dashboard&token=${this.token}`
+  data() {
+    return {
+      orders: []
     }
+  },
+  methods: {
+    getOrders(page, limit) {
+      axios.get('/admin/index.php?route=sale/order/getOrders', {
+        params: {
+          token: this.token,
+          page,
+          limit
+        }
+      }).then(res => {
+        this.orders = res
+      })
+    }
+  },
+  mounted() {
+    this.getOrders(1, 10)
+    this.getOrders(2, 10)
   }
 }
 </script>
